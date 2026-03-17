eeaba5a06e961811bf4c93465a37756fa8615421
Harsh <hr239531@gmail.com>
2026-03-17 16:56:45 +0530

    minor changes

--- backend/controllers/admins/admin.controller.js
+++ backend/controllers/admins/admin.controller.js
@@ -260,6 +260,7 @@ const getAdminDashboardData = async (req, res) => {
               "$tuitionFee",
               "$hostelFee",
               "$transportFee",
+              "$admissionFee",
               "$examFee"
             ]
           },
